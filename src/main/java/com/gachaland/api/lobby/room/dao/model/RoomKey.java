7919d50b9ec4bbd283c46415126510121baa7249
chris.j3 <chris.j3@kakaocorp.com>
2017-07-19 00:49:18 +0900

    복합키

--- src/main/java/com/gachaland/api/lobby/room/dao/model/RoomKey.java
+++ src/main/java/com/gachaland/api/lobby/room/dao/model/RoomKey.java
@@ -2,11 +2,13 @@ package com.gachaland.api.lobby.room.dao.model;
 
 import lombok.Data;
 
+import java.io.Serializable;
+
 /**
  * Created by jhpark1220 on 2017. 7. 19..
  */
 @Data
-public class RoomKey {
+public class RoomKey implements Serializable {
     private long roomId;
     private long memberId;
 }
